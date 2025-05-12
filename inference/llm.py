38d93bf0aa831ffd29c569dc667587a5f59b4663
Guillaume Baudart <guillaume.baudart@inria.fr>
2025-05-12 14:21:16 +0200

    WIP batch

--- inference/llm.py
+++ inference/llm.py
@@ -55,16 +55,17 @@ class VLLM(LLM):
         if stop_sequences:
             payload["stop"] = stop_sequences
 
-        response = requests.post(
-            f"{self.api_url}/v1/completions",
-            headers={"Content-Type": "application/json"},
-            data=json.dumps(payload),
-        )
-
-        if response.status_code != 200:
-            raise Exception(
-                f"LLM API returned error: {response.status_code} - {response.text}"
+        for _ in range(32):
+            response = requests.post(
+                f"{self.api_url}/v1/completions",
+                headers={"Content-Type": "application/json"},
+                data=json.dumps(payload),
             )
+    
+            if response.status_code != 200:
+                raise Exception(
+                    f"LLM API returned error: {response.status_code} - {response.text}"
+                )
 
         llm_response = response.json()["choices"][0]["text"][len_prompt:]
         if self.verbose:
