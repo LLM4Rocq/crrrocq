5b9fb44bea9b386ba95f08eaa8e91f81dfaec641
Marc Lelarge <marc.lelarge@inria.fr>
2025-05-19 17:00:28 +0200

    prompt

--- inference/llm.py
+++ inference/llm.py
@@ -68,7 +68,7 @@ class VLLM(LLM):
         Returns:
             The constructed prompt
         """
-        return tactic_prompts.basisc_prompts.format(
+        return tactic_prompts.basic_prompt.format(
             coq_tag=coq_tag, goals_tag=goals_tag, goals=goals, context=context
         )
 
