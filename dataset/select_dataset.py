cbfe494f662e08713dd84871e3cb85f7fee82aa7
Jules Viennot Franca <jules.viennot@gmail.com>
2025-05-26 13:22:30 +0200

    Selecting the dataset update

--- dataset/select_dataset.py
+++ dataset/select_dataset.py
@@ -28,22 +28,22 @@ def select_have(theorems, total, share, select_with, select_without):
     """Select the theorems with have."""
     assert (0 <= share <= 1)
 
-    with_have = [t for t, theorem in enumerate(theorems) if theorem["proof"].find("(*<have>*)") >= 0]
-    with_have = [(extract_have_proofs(theorems[t]), t) for t in with_have]
-    with_have = [(list(map(number_of_tactics_have_proof, haves)), t) for haves, t in with_have]
-    with_have = [(sum(lnot) / len(lnot), len(lnot), theorems[t]["score"], t) for lnot, t in with_have]
+    with_have = [qn for qn, theorem in theorems.items() if theorem["proof"].find("(*<have>*)") >= 0]
+    with_have = [(extract_have_proofs(theorems[qn]), qn) for qn in with_have]
+    with_have = [(list(map(number_of_tactics_have_proof, haves)), qn) for haves, qn in with_have]
+    with_have = [(sum(lnot) / len(lnot), len(lnot), theorems[qn]["score"], qn) for lnot, qn in with_have]
     max_mean_haves_size = max(*[m for m, _, _, _ in with_have])
     max_nbr_haves       = max(*[n for _, n, _, _ in with_have])
     max_scores          = max(*[s for _, _, s, _ in with_have])
-    with_have = [(m/max_mean_haves_size + n/max_nbr_haves + s/max_scores, t) for m, n, s, t in with_have]
+    with_have = [(m/max_mean_haves_size + n/max_nbr_haves + s/max_scores, qn) for m, n, s, qn in with_have]
     with_have.sort()
     with_have = list_selectors[select_with](with_have, int(total * share))
 
-    without_have = [(theorem["score"], t) for t, theorem in enumerate(theorems) if theorem["proof"].find("(*<have>*)") < 0]
+    without_have = [(theorem["score"], qn) for qn, theorem in theorems.items() if theorem["proof"].find("(*<have>*)") < 0]
     without_have.sort()
     without_have = list_selectors[select_without](without_have, total - len(with_have))
 
-    return [theorems[t] for _, t in with_have] + [theorems[t] for _, t in without_have]
+    return {qn: theorems[qn] for _, qn in with_have} | {qn:theorems[qn] for _, qn in without_have}
 
 selectors = {
     "1000_0.5_first_19-20th": lambda theorems: select_have(theorems, 1_000, 0.5, "first", "19-20th")
@@ -55,7 +55,7 @@ def make(dataset, selector):
     datafile = Path(dataset)
     if not datafile.exists():
         raise Exception(f"Error: {dataset} doesn't exist.")
-    savefile = Path(datafile.parent, datafile.stem + "_" + selector + ".jsonl")
+    savefile = Path(datafile.parent, datafile.stem + "_" + selector + ".json")
 
     if savefile.exists():
         print("Selected dataset already here.")
@@ -64,25 +64,21 @@ def make(dataset, selector):
         print("Making the selected dataset:")
 
         print("  Download all the theorems ...")
-        theorems = []
         with open(datafile, "r") as f:
-            for line in f:
-                theorems.append(json.loads(line))
+            theorems = json.load(f)
 
         print("  Select theorems ...")
         theorems = selectors[selector](theorems)
 
         print("  Saving the selected data ...")
         with open(savefile, "w") as f:
-            for theorem in theorems:
-                theorem = json.dumps(theorem)
-                f.write(theorem + '\n')
+            json.dump(theorems, f, indent=2)
 
         print("  DONE!")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Select theorems in a dataset.")
-    parser.add_argument("--dataset", type=str, default="math-comp_bm25_have.jsonl", help="The path of the dataset, default is 'math-comp_bm25_have.jsonl'")
+    parser.add_argument("--dataset", type=str, default="mathcomp_bm25_have.json", help="The path of the dataset, default is 'mathcomp_bm25_have.json'")
     parser.add_argument("--selector", type=str, default="1000_0.5_first_19-20th", help="The selection algorithm to use, it can be chosen among: " + ", ".join(selectors.keys()))
     args = parser.parse_args()
     make(args.dataset, args.selector)
