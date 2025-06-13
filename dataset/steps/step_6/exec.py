7166ec4a0efd459480805fdbe81f9fa8f1c24d36
theostos <theo.stoskopf@proton.me>
2025-06-13 10:34:44 +0200

    Initial step 6: parser + faiss index

---
+++ dataset/steps/step_6/exec.py
@@ -0,0 +1,66 @@
+import argparse
+import json
+import os
+from copy import deepcopy
+import re
+
+from tqdm import tqdm
+
+from src.embedding_models.gteqwen import GteQwenEmbedding
+from src.embedding_models.mxbai import MxbaiEmbedding
+from src.embedding_models.qwen_embedding import Qwen3Embedding600m, Qwen3Embedding4b, Qwen3Embedding8b
+from src.index.cosim_index import FaissIndex
+
+DICT_MODEL = {
+    "gte_qwen": GteQwenEmbedding,
+    "mxbai": MxbaiEmbedding,
+    "qwen_embedding_600m": Qwen3Embedding600m,
+    "qwen_embedding_4b": Qwen3Embedding4b,
+    "qwen_embedding_8b": Qwen3Embedding8b,
+}
+
+def parse_output(output: str):
+    pattern = re.compile(
+        r"<(?P<kind>\w+)>\s*(?P<content>.*?)\s*</\1>",
+        re.DOTALL | re.MULTILINE,
+    )
+    blocks = []
+    for m in pattern.finditer(output):
+        blocks.append({
+            "kind": m.group("kind"),
+            "content": m.group("content").strip()
+        })
+
+    return blocks
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input',  default='export/output/steps/step_5/result.json')
+    parser.add_argument('--dictionary', default='export/docstrings/dictionary.json', help='Database path')
+    parser.add_argument('--output',  default='export/output/steps/step_6/')
+    parser.add_argument('--model-name', default='mxbai', help="Embedding model's name")
+    parser.add_argument('--device', default='cpu', help="Device for embedding model")
+    parser.add_argument('--batch-size', default=32, help="Batch size used to pre compute embedding")
+    parser.add_argument('--top-k', default=10, help="Top-k parameter use for retrieval", type=int)
+    args = parser.parse_args()
+
+    os.makedirs(args.output, exist_ok=True)
+    with open(args.dictionary, 'r') as file:
+        dictionary = json.load(file)
+    
+    with open(args.input, 'r') as file:
+        content = json.load(file)
+    
+    model = DICT_MODEL[args.model_name](device=args.device)
+    index = FaissIndex(model, dictionary, batch_size=args.batch_size)
+
+    for entry in content:
+        blocks = parse_output(entry['output'])
+        for block in blocks:
+            if block['kind'] == 'search':
+                search_result = index.query(block['content'], top_k=args.top_k)
+
+    with open(os.path.join(args.output, 'result.json'), 'w') as file:
+        json.dump(content, file, indent=4)
+    
+    # score = index.query(query, top_k=args.top_k)
