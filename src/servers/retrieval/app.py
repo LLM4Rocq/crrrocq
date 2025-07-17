b36e05a0fd78153b336b0700ba1272d64a319e41
theostos <theo.stoskopf@proton.me>
2025-07-17 13:40:37 +0200

    Add ip_path, and dynamic file management

--- src/servers/retrieval/app.py
+++ src/servers/retrieval/app.py
@@ -1,4 +1,6 @@
 import json
+import time
+import os
 
 import yaml
 from flask import Flask, request, jsonify
@@ -14,7 +16,14 @@ with open('config/server/retrieval/config.yaml', 'r') as file:
 with open(config['docstrings_path'], 'r') as file:
     dictionary = json.load(file)
 
-embedding = OpenAIEmbedding(config['model_name'])
+while not os.path.exists(config['ip_path']):
+    time.sleep(20)
+
+with open(config['ip_path'], 'r') as file:
+    ip = file.read()
+    base_url = f"http://{ip}/v1"
+
+embedding = OpenAIEmbedding(config['model_name'], base_url=base_url)
 index = FaissIndex(embedding, content=dictionary, cache_path=config['cache_path'])
 
 @app.route('/query', methods=['POST'])
