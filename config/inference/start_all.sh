ddfe10b6d58fce8cb28326f0772c4d8cf719e5cc
theostos <theo.stoskopf@proton.me>
2025-07-17 15:57:30 +0200

    Update sleep time

---
+++ config/inference/start_all.sh
@@ -0,0 +1,28 @@
+ID=$RANDOM_$(date '+%Y-%m-%d %H:%M:%S')
+
+export RETRIEVAL_IP_PATH=/lustre/fsn1/projects/rech/tdm/commun/retrieval_ip_$ID.txt
+export PET_IP_PATH=/lustre/fsn1/projects/rech/tdm/commun/pet_ip_$ID.txt
+export MODEL_IP_PATH=/lustre/fsn1/projects/rech/tdm/commun/model_ip_$ID.txt
+export EMBED_IP_PATH=/lustre/fsn1/projects/rech/tdm/commun/embed_ip_$ID.txt
+
+rm -f $RETRIEVAL_IP_PATH
+rm -f $PET_IP_PATH
+rm -f $MODEL_IP_PATH
+rm -f $EMBED_IP_PATH
+
+sbatch --export=ALL sglang.slurm
+while [ ! -f "$MODEL_IP_PATH" ] || [ ! -f "$EMBED_IP_PATH" ]; do
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] Waiting for SGLang"
+    sleep 20
+done
+
+sbatch --export=ALL pet.slurm
+sbatch --export=ALL retrieval.slurm
+while [ ! -f "$RETRIEVAL_IP_PATH" ] || [ ! -f "$PET_IP_PATH" ]; do
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] Waiting for retrieval and pet servers"
+    sleep 20
+done
+
+
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] Everything ready."
