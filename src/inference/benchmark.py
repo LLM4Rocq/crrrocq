b3df59a0f6584316b01bc6388c23727bd5fcdb7d
Marc Lelarge <marc.lelarge@inria.fr>
2025-07-17 16:24:23 +0200

    bench

--- src/inference/benchmark.py
+++ src/inference/benchmark.py
@@ -222,7 +222,7 @@ def main():
     )
     parser.add_argument("--file", type=str, default="foo.v", help="Coq file name")
     parser.add_argument(
-        "--theorem", type=str, required=True, help="Name of the theorem to prove"
+        "--theorem", type=str, required=False, help="Name of the theorem to prove"
     )
     parser.add_argument(
         "--host", type=str, default="127.0.0.1", help="Pytanque server host"
