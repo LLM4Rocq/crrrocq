fdeb4248f57e19689f1fe554453734db5720271f
theostos <theo.stoskopf@proton.me>
2025-07-18 18:49:07 +0200

    Quick test for inference script

--- script/inference.py
+++ script/inference.py
@@ -13,13 +13,11 @@ from src.inference.agent import MathAgent, MathAgentError
 def try_proof(agent_config, thm_name, export_dir, id):
     agent = MathAgent(agent_config)
     agent.start_thm(thm_name)
-    try:
-        agent.run_proof()
-        filepath = os.path.join(export_dir, f'SUCCESS_{thm_name}_{id}.json')
-        result = {"status": "success"}
-    except MathAgentError as e:
-        filepath = os.path.join(export_dir, f'FAIL_{thm_name}_{id}.json')
-        result = {"status": "fail"}
+    agent.run_proof()
+    filepath = os.path.join(export_dir, f'SUCCESS_{thm_name}_{id}.json')
+    result = {"status": "success"}
+    filepath = os.path.join(export_dir, f'FAIL_{thm_name}_{id}.json')
+    result = {"status": "fail"}
     
     result = agent.export_result() | result
     with open(filepath, 'w') as file:
@@ -37,12 +35,12 @@ def main():
     parser.add_argument(
         "--pass-k",
         type=int,
-        default=4096
+        default=32
     )
     parser.add_argument(
         "--max-workers",
         type=int,
-        default=256
+        default=32
     )
     parser.add_argument(
         "--evaluation-file",
@@ -79,6 +77,8 @@ def main():
     
     futures = []
     for thm_name in tqdm(thm_names, desc="Theorems", position=0):
+        try_proof(config, thm_name, folder_path, 0)
+        exit()
         with concurrent.futures.ProcessPoolExecutor(max_workers=args.max_workers) as executor:
             for i in range(args.pass_k):
                 futures.append(executor.submit(try_proof, config, thm_name, folder_path, i))
